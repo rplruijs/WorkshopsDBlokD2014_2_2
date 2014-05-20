ade2ec7f95c67975534a96ec63c7d1e6dbe51875
Remco <remcoruijsenaars@gmail.com>
2014-05-20 21:01:06 +0200

    Change comments

---
+++ src/main/java/workshop7/ZuulBetter/ZuulStart.java
@@ -0,0 +1,20 @@
+package workshop7.ZuulBetter;/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Alwine
+ */
+public class ZuulStart {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     Game game = new Game();
+     game.play();
+    
+    }
+}
