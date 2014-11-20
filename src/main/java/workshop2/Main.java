8cd7025dc0fef27abe0b042d5ac700826e164a58
Remco <remcoruijsenaars@gmail.com>
2014-11-20 10:46:17 +0100

    Change comments

--- src/main/java/workshop2/Main.java
+++ src/main/java/workshop2/Main.java
@@ -11,10 +11,20 @@ public class Main {
 
     public static void main(String[] args){
 
+        Student s1 = new Student("9807926" , "Remco");
+        Student s2 = new Student("1020344" , "Guus");
+        double c1 = 10; double c2 = 8.4;
+        TentamenResultaat t1 = new TentamenResultaat(c1, s1);
+        TentamenResultaat t2 = new TentamenResultaat(c2, s2);
+        t1 = t2;
+        s2.setNaam("Guzie");
+        c2 = 5.6;
+        System.out.println(t1);
+        System.out.println(t2);
 
+        Student s = new Student("8787234", "Tom");
 
-    //     Student s3 = new Student();
-
+        System.out.println(s);
 
     }
 }
